c69c9c6296e06179567872db325463f4556e59bf
MaheshBoopathiAP <727821tuit055@gmail.com>
2024-05-22 06:03:57 +0530

    initial commit

---
+++ node/modules.js
@@ -0,0 +1,5 @@
+const people = require('./people');
+const xyz = require('./people');
+
+console.log(xyz.people);
+console.log(xyz.ages);
---
+++ node/people.js
@@ -0,0 +1,9 @@
+const people = ['mahesh', 'mario', 'hainguyen', 'don-lee'];
+const ages = [20, 25, 30, 35];
+
+console.log(people);
+
+module.exports = {
+    people,
+    ages
+};
