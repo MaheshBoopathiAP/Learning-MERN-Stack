6cf1a663e387ca8e87644c1495f58987214f722a
MaheshBoopathiAP <727821tuit055@gmail.com>
2024-05-24 01:35:20 +0530

    day-2

--- node/server.js
+++ node/server.js
@@ -1,16 +1,60 @@
 const http = require('http');
-const { type } = require('os');
+// const { type } = require('os');
+const fs = require('fs');
+const _ = require('lodash');
+
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
 
-    //set header ontent type
+    //lodash
+    const num = _.random(0,20);
+    console.log(num);
+
+    const greet = _.once(() => {
+        console.log("hello");
+    });
+
+    greet();
+    greet();
+
+    //set header content type
     res.setHeader('content-Type','text/html');
 
-    res.write('<head><link rel="stylesheet" href="#"></head>');
-    res.write('<p>hello, ninjas</p>');
-    res.write('<p>hello again, ninjas</p>');
-    res.end();
+    let path = './views/';
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            res.statusCode = 200;
+            break;
+        case '/about':
+            path += 'about.html';
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location','/about');
+            res.end();
+            break;
+        default:
+            path += '404.html';
+            res.statusCode = 404;
+            break;
+
+    }
+
+    //send a html file
+    fs.readFile(path,(err,data) => {
+        if(err){
+            console.log(err);
+            res.end();
+        }
+        else{
+            // res.write(data);
+            res.end(data);
+        }
+    });
+
+    
 });
 
 server.listen(3000,'localhost',() => {
